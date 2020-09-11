b7eb5d33d7c86476cdffb72045aeac2667675925
vishaljagtap3 <vishal.jagtap@bitcode.in>
2020-09-11 14:03:47 +0530

    ListView 2 with custom AdapterImages

--- app/src/main/java/in/bitcode/listview2/AdapterImages.java
+++ app/src/main/java/in/bitcode/listview2/AdapterImages.java
@@ -1,6 +1,7 @@
 package in.bitcode.listview2;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,30 +13,46 @@ public class AdapterImages extends BaseAdapter {
 
     private Context mContext;
     private int [] mArrImageIds;
+    private boolean [] mArrImageStates;
 
-    public AdapterImages( Context context, int [] arrImageIds ){
+    public AdapterImages( Context context, int [] arrImageIds, boolean [] arrImageStates  ){
         mContext = context;
         mArrImageIds = arrImageIds;
+        mArrImageStates = arrImageStates;
     }
 
     @Override
     public int getCount() {
-
         Log.e("listview2", "getcount called");
-
         return mArrImageIds.length;
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
 
-        Log.e("listview2", "getView called for " + position);
+        ImageView imageView;
 
-        ImageView imageView = new ImageView( mContext );
-        imageView.setLayoutParams( new ListView.LayoutParams( 400 , 200 ) );
-        imageView.setPadding(20, 10, 20, 10);
+        if( convertView == null ) {
+            //create new ImageView
+            Log.e("listview2", "new view created for " + position);
+            imageView = new ImageView( mContext );
+            imageView.setLayoutParams( new ListView.LayoutParams( 400 , 200 ) );
+            imageView.setPadding(20, 10, 20, 10);
+        }
+        else {
+            //Reuse the convert view
+            Log.e("listview2", "convert view reused for " + position);
+            imageView = (ImageView) convertView;
+        }
 
-        imageView.setImageResource( mArrImageIds[position] );
+        imageView.setImageResource(mArrImageIds[position]);
+        if( mArrImageStates[position] == true ) {
+            imageView.setBackgroundColor(Color.rgb(200, 100, 100));
+        }
+        else {
+            imageView.setBackgroundColor( Color.rgb(255, 255, 255));
+        }
+
 
         return imageView;
     }
